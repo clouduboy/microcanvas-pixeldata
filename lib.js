f90f70311e4e894854f3adaa72045a0189ed246e
Flaki <git@flaki.hu>
2015-11-03 15:53:28 +0100

    BMP import, multiple extensions, sprite directory grouping support

--- lib.js
+++ lib.js
@@ -3,7 +3,21 @@
 (function() {
 
 function PixelData(input) {
-  this.bitmap;
+  // Buffer object - should be some binary image file.
+  // Supported import formats: BMP
+  if (typeof input === 'object' && typeof Buffer !== 'undefined' && input instanceof Buffer) {
+    try {
+      this.bitmap = bmp2bitmap(input);
+      this.w = this.bitmap[0].length;
+      this.h = this.bitmap.length;
+      return;
+    } catch(e) {
+
+    }
+
+    // Not any of the supported binary formats, convert to string
+    input = input.toString();
+  }
 
   // Could be serialized JSON
   if (typeof input === 'string') {
@@ -57,6 +71,7 @@ function PixelData(input) {
     return;
   }
 
+  throw("Unrecognized file format.");
 }
 
 PixelData.prototype = {
@@ -171,6 +186,69 @@ function bytes2bitmap(bytes,w,h) {
 
 function bytes2pif(bytes,w,h,id) {
   return bitmap2pif( bytes2bitmap(bytes,w,h), id );
+}
+
+// Import BMP bitmap files
+// Based on the BMP data format structure documented in:
+// http://paulbourke.net/dataformats/bmp/
+function bmp2bitmap(b) {
+  // BM (magic identifier) in fist two bytes: 0x42@0, 0x4D@1
+  if (b[0] !== 0x42 || b[1] !== 0x4D) throw("Not a valid BMP file.");
+
+  // Image data starts: 4-byte-uint@10
+  var imgdata = b.readUInt32LE(10);
+
+  // Image width & height: 4-byte-int@18,22
+  var w = b.readInt32LE(18),
+      h = b.readInt32LE(22);
+
+  // Bits-per-pixel: 2-byte-ushort@28
+  // 32bpp means alpha channel + RGB
+  var bpp = b.readUInt16LE(28);
+
+  // Log import
+  console.log("Importing BMP bitmap:", b.length,"bytes,",w,"x",h,"bitmap @",bpp,"bpp");
+
+  // Read image bitmap based on above metadata
+  var bitmap = new Array(h),
+      y = 0, v;
+
+  for (i = 0; i < w*h; ++i) {
+    // For some reason the image rows are flipped in BMP files
+    y = h - 1 - Math.floor(i/w);
+
+    // New row
+    if (!bitmap[y]) bitmap[y] = [];
+
+    switch (bpp) {
+      // 32 bit ABGR
+      case 32:
+        v = b.readUInt32LE(imgdata + i * 4);
+
+        // Check transparency value, also non-black color value
+        bitmap[y][i%w] = ( (v >>> 24) > (0xFF >> 1) && (v & 0xFFFFFF) ) ? 1 : 0;
+        break;
+
+      // 24 bit BGR
+      case 24:
+        v = b[imgdata + i*3] | b[imgdata + i*3 + 1] | b[imgdata + i*3 + 2];
+
+        // Count all non-completely-black color values
+        bitmap[y][i%w] = v ? 1 : 0;
+        break;
+
+      // 1 bit B&W
+      case 1:
+        // Strange padding to 4 bytes
+        v = b.readUInt32BE(imgdata + (h - y - 1) * Math.ceil(w/32)*4);
+
+        // Count all bits
+        bitmap[y][i%w] = ( (v >>> (31-(i%w))) & 1 ) ? 1 : 0;
+        break;
+    }
+  }
+
+  return bitmap;
 }
 
 /*
